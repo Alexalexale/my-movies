9abb60d4e20dcabdc5523c1c219388947cc5d02e
ruby 31 <ruby.31@metralha.cec.ime.usp.br>
2017-01-24 21:35:04 -0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+movies = [
+  {title: "Alladin", release_date: '25-11-1992'},
+  {title: "When Harry Met sally", release_date: '25-08-1992'},
+  {title: "The Help", release_date: '25-4-1992'},
+  {title: "Raiders of the Lost Ark", release_date: '25-01-1992'}
+]
+
+movies each do |movie|
+  Movie.create!(movie)
+end
