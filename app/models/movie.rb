f85e07daad9bd8ecb08f5fa978ffeca32783f82f
ruby 31 <ruby.31@metralha.cec.ime.usp.br>
2017-01-27 21:04:10 -0200

    Validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+  validates :title,:release_date, presence: true
+  validates :title, uniqueness: { scope: :release_date,
+    message: "should not have two movies with same name and release date"}
 end
